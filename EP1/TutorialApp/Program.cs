7f8fbb31071d4b3e3b5965695624e4aa0c532e4f
UPDOWNUP-64BIT <80029104+UPDOWNUP-64BIT@users.noreply.github.com>
2021-07-11 00:43:37 -0230

    Add files via upload

---
+++ EP1/TutorialApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace TutorialApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // String's and integer's in a nutshell c# tutorial S1 EP1
+            string url = "google.com";
+            int version = 1;
+            Console.WriteLine($"Current Version: {version} Latest download link {url}");
+            Thread.Sleep(2000);
+            version = 2;
+            url = "youtube.com";
+            Console.WriteLine($"Current Version: {version} Latest download link {url}");
+        }
+    }
+}
